7b10d10771e3a7ca4bcef7169200cb4d3da0d3cd
LAPTOP-FVG63UF4\andre <andrescastano93@gmail.com>
2021-09-30 19:10:29 +0200

    ultimes modificacions

--- src/es/florida/AE01_Ficheros/App.java
+++ src/es/florida/AE01_Ficheros/App.java
@@ -2,7 +2,7 @@ package es.florida.AE01_Ficheros;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
+import java.util.Scanner;
 
 
 public class App {
@@ -15,13 +15,47 @@ public class App {
 	
 	public static void main(String[] args) {
 	
-
-		
-		
+		File directorio = new File(".");
+		String respuesta;
+		System.out.println("INFORMACION: ");
+		getInformacion(directorio);
+		gestioErrors(directorio);
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Deseas crear una carpeta o un fichero?: ");
+		respuesta=teclado.next();
+		if(respuesta=="Carpeta"||respuesta=="carpeta") {
+			crearCarpeta(directorio);
+			gestioErrors(directorio);
+		}else if(respuesta=="Fichero"||respuesta=="fichero") {
+			crearFichero(directorio);
+		}else if(respuesta=="No"||respuesta=="no") {
+			System.out.print("Deseas borrar el archivo?: ");
+			respuesta=teclado.next();
+			if(respuesta=="Si"||respuesta=="si") {
+				elimina(directorio);
+				gestioErrors(directorio);
+			}else if(respuesta=="No"||respuesta=="no") {
+				System.out.print("Deseas renombrar el archivo?: ");
+				respuesta=teclado.next();
+				if(respuesta=="Si"||respuesta=="si") {
+					renombra(directorio);
+					gestioErrors(directorio);
+				}else if(respuesta=="No"||respuesta=="no") {
+					System.out.println("No has realizado ningun cambio");
+				}else {
+					System.out.println("No he entendido la orden. ");
+				}
+			}else {
+				System.out.println("No he entendido la orden. ");
+			}
+		}else {
+			System.out.println("No he entendido la orden. ");
+		}
+		teclado.close();
 	}
 	
-	public void getInformacion() {
-		File fichero = new File("Ejemplo_T0_File.txt");
+	public static void getInformacion(File fichero) {
+		
 		Fichero fichero1 = new Fichero();
 		fichero1.setNombre(fichero.getName());
 		fichero1.setTipo(fichero.isDirectory());
@@ -30,12 +64,12 @@ public class App {
 		fichero1.setOculto(fichero.isHidden());
 		if(fichero1.getTipo()==false) {
 			fichero1.setTamanyo(fichero.length());
-			System.out.println(fichero1.getNombre());
+			System.out.println("El nombre del fichero es: "+fichero1.getNombre());
 			System.out.println("Es directorio");
-			System.out.println(fichero1.getUbi());
-			System.out.println(fichero1.getFecha());
-			System.out.println(fichero1.getOculto());
-			System.out.println(fichero1.getTamanyo());
+			System.out.println("Su ubicacion es: "+fichero1.getUbi());
+			System.out.println("La ultima modificacion fue: "+fichero1.getFecha());
+			System.out.println("Es un archivo oculto?: "+fichero1.getOculto());
+			System.out.println("Su tamaño es de "+fichero1.getTamanyo()+" bytes.");
 		}else {
 			String[] lista=fichero.list();
 			for(String elemento : lista) {
@@ -44,20 +78,20 @@ public class App {
 			fichero1.setEspacioLibre(fichero.getFreeSpace());
 			fichero1.setEspacioDisponible(fichero.getUsableSpace());
 			fichero1.setEspacioTotal(fichero.getTotalSpace());
-			System.out.println(fichero1.getNombre());
+			System.out.println("El nombre del fichero es: "+fichero1.getNombre());
 			System.out.println("Es fitxer");
-			System.out.println(fichero1.getUbi());
-			System.out.println(fichero1.getFecha());
-			System.out.println(fichero1.getOculto());
-			System.out.println(fichero1.getElementos());
-			System.out.println(fichero1.getEspacioLibre());
-			System.out.println(fichero1.getEspacioDisponible());
-			System.out.println(fichero1.getEspacioTotal());
+			System.out.println("Su ubicacion es: "+fichero1.getUbi());
+			System.out.println("La ultima modificacion fue: "+fichero1.getFecha());
+			System.out.println("Es un archivo oculto?: "+fichero1.getOculto());
+			System.out.println("Estos son sus elementos: "+fichero1.getElementos());
+			System.out.println("El espacio libre es de "+fichero1.getEspacioLibre()+" bytes");
+			System.out.println("El espacio disponible es de: "+fichero1.getEspacioDisponible()+" bytes");
+			System.out.println("El espacio total es de: "+fichero1.getEspacioTotal()+" bytes");
 		}
 	}
 	
-	public void crearCarpeta() {
-		File directorio = new File(".");
+	public static void crearCarpeta(File directorio) {
+	
 		if(!directorio.exists()) {
 			if(directorio.mkdirs()) {
 				System.out.println("Directorio creado. ");
@@ -67,8 +101,8 @@ public class App {
 		}
 	}
 	
-	public void crearFichero() {
-		File fichero = new File("C:\\Users\\andre\\eclipse-workspace\\AE01_Ficheros\\bin\\es\\florida\\AE01_Ficheros");
+	public static void crearFichero(File fichero) {
+		fichero = new File("C:\\Users\\andre\\eclipse-workspace\\AE01_Ficheros\\bin\\es\\florida\\AE01_Ficheros");
 		try {
 			if(fichero.createNewFile()) {
 				System.out.println("Fichero creado correctamente. ");
@@ -89,8 +123,8 @@ public class App {
 		}
 	}
 	
-	public void renombra() {
-		File f1 = new File("fichero1.txt");
+	public static void renombra(File f1) {
+		 f1 = new File("fichero1.txt");
 		File f2 = new File("fichero2.txt");
 		if(f1.renameTo(f2)) {
 			System.out.println("Archivo renombrado correctamente. ");
